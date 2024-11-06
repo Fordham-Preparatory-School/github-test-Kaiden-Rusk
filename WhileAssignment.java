ba670251532ecbc92d7aed01001a7bdec3cf2777
Kaiden-Rusk <ruskk26@fordhamprep.org>
2024-11-06 13:29:26 -0500

    Add files via upload

---
+++ WhileAssignment.java
@@ -0,0 +1,12 @@
+public class WhileAssignment {
+    public static void main (String [] args)
+    {
+        int num = 2;
+        while (num<=300)
+        {
+            System.out.println("The vaule of num is :"+ num);
+            num *=2;
+            num++;
+        }
+    }
+}
